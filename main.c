d8691ecb201d74d83184395ec23725f439bb32d7
cemer99 <92156523+cemer99@users.noreply.github.com>
2021-11-06 22:38:54 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int son,num,i;
+    printf("Kaca kadar olacagini yaziniz: ");
+    scanf("%d",&son);
+
+    for(i=0;i<=son;i+=2)
+    {
+        printf("%d\n",i);
+    }
+
+
+
+
+
+
+    return 0;
+}
